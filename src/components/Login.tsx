563e9f8852d57adb8e4e15e364273c6c84a33de1
gvillalb <gianluca.nadir.villalba@capgemini.com>
2021-11-30 12:57:11 +0100

    code clenup

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { signup, login, logout, useAuth } from "../firebase";
+import { signup, login } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { onSaveUser } from "../userReducer";
@@ -7,7 +7,6 @@ import { onSaveUser } from "../userReducer";
 function Login({ currentUser }) {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  // const currentUser = useAuth();
   const dispatch = useDispatch();
 
   const emailRef = useRef<HTMLInputElement>(null);
@@ -43,30 +42,17 @@ function Login({ currentUser }) {
     setLoading(false);
   }
 
-  async function handleLogOut() {
-    setLoading(true);
-    try {
-      logout();
-    } catch (error) {
-      alert(error);
-    }
-    setLoading(false);
-  }
-
   return (
     <div id="main">
       <div id="fields">
         <input ref={emailRef} placeholder="Email" />
-        <input ref={passwordRef} type="password" placeholder="Password" />
+        <input id="password" ref={passwordRef} type="password" placeholder="Password" />
       </div>
       <button disabled={loading || currentUser} onClick={handleSignup}>
         Sign Up
       </button>
-      <button disabled={loading || currentUser} onClick={handlelogIn}>
+      <button id="login" disabled={loading || currentUser} onClick={handlelogIn}>
         Log In
-      </button>
-      <button disabled={loading || !currentUser} onClick={handleLogOut}>
-        Log Out
       </button>
     </div>
   );
