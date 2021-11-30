df1753824e61007379f33cfa58d2b314dad19d7c
gvillalb <gianluca.nadir.villalba@capgemini.com>
2021-11-30 17:28:33 +0100

    restyling using material UI

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,6 +4,13 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { onSaveUser } from "../userReducer";
 import { RootState } from "..";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 
 function Login() {
   const [loading, setLoading] = useState(false);
@@ -44,27 +51,52 @@ function Login() {
   }
 
   return (
-    <div id="main">
-      <div id="fields">
-        <input ref={emailRef} placeholder="Email" />
-        <input
-          id="password"
-          ref={passwordRef}
-          type="password"
-          placeholder="Password"
+    <Card>
+      <CardContent>
+        <div id="card-title">
+          <Typography>Sign In</Typography>
+          <Divider light />
+        </div>
+        <TextField
+          id="input-email"
+          label="Email"
+          type="text"
+          variant="standard"
         />
-      </div>
-      <button disabled={loading || !!currentUser} onClick={handleSignup}>
-        Sign Up
-      </button>
-      <button
-        id="login"
-        disabled={loading || !!currentUser}
-        onClick={handlelogIn}
-      >
-        Log In
-      </button>
-    </div>
+        <TextField
+          id="input-password"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="standard"
+        />
+      </CardContent>
+      <CardActions id="buttons">
+        <Button size="small">Sign Up</Button>
+        <Button size="small">Login</Button>
+      </CardActions>
+    </Card>
+    // <div id="main">
+    //   <div id="fields">
+    //     <input ref={emailRef} placeholder="Email" />
+    //     <input
+    //       id="password"
+    //       ref={passwordRef}
+    //       type="password"
+    //       placeholder="Password"
+    //     />
+    //   </div>
+    //   <button disabled={loading || !!currentUser} onClick={handleSignup}>
+    //     Sign Up
+    //   </button>
+    //   <button
+    //     id="login"
+    //     disabled={loading || !!currentUser}
+    //     onClick={handlelogIn}
+    //   >
+    //     Log In
+    //   </button>
+    // </div>
   );
 }
 
