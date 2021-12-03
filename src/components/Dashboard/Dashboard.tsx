5d24fc039192d084aedfa2d582a094b03dec32e0
gvillalb <gianluca.nadir.villalba@capgemini.com>
2021-12-03 01:46:53 +0100

    work on dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -4,23 +4,40 @@ import { logout } from "../../firebase";
 import { onLogout } from "../../userReducer";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import { CustomAppBar } from "./DashboardStyle";
+import CardContent from "@mui/material/CardContent";
+import SettingsIcon from "@mui/icons-material/Settings";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import {
+  CustomAppBar,
+  CustomCard,
+  CustomCardHeader,
+  CustomStack,
+  CustomButton,
+  CustomToolbar,
+} from "./DashboardStyle";
 
 const Dashboard = () => {
   const userEmail = useSelector<RootState>((state) => state.user.email);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
   useEffect(() => {
     console.log("dashboard", userEmail);
   }, []);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   async function handleLogOut() {
     try {
@@ -34,32 +51,50 @@ const Dashboard = () => {
   }
 
   return (
-    <Box>
-      <CustomAppBar>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Dashboard
-          </Typography>
-          <Button color="inherit" onClick={handleLogOut}>
-            Log Out
-          </Button>
-        </Toolbar>
-      </CustomAppBar>
-    </Box>
-    // <div>
-    //   <div>Currently logged in as {userEmail}</div>
-    //   <button id="logout" onClick={handleLogOut}>
-    //     Log Out
-    //   </button>
-    // </div>
+    <>
+      <Box>
+        <CustomAppBar>
+          <CustomToolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              dashboard
+            </Typography>
+            <Button
+              id="basic-button"
+              aria-controls="basic-menu"
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              Dashboard
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={handleLogOut}>Log Out</MenuItem>
+            </Menu>
+            {/* <Button color="inherit" onClick={handleLogOut}>
+              Log Out
+            </Button> */}
+          </CustomToolbar>
+        </CustomAppBar>
+      </Box>
+      <CustomCard>
+        <CustomCardHeader title="Secret Dashboard"></CustomCardHeader>
+        <CardContent>Dashboard secrets</CardContent>
+      </CustomCard>
+      <CustomStack>
+        <CustomButton startIcon={<DashboardIcon />}>Dashboard</CustomButton>
+        <CustomButton className="settings-button" startIcon={<SettingsIcon />}>
+          Settings
+        </CustomButton>
+      </CustomStack>
+    </>
   );
 };
 
--- src/components/Dashboard/DashboardStyle.ts
+++ src/components/Dashboard/DashboardStyle.ts
@@ -1,9 +1,53 @@
-import Toolbar from "@mui/material/Toolbar";
 import AppBar from "@mui/material/AppBar";
 import styled from "styled-components";
+import Card from "@mui/material/Card";
+import { CardHeader } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
 
 export const CustomAppBar = styled(AppBar)`
   background: transparent;
-	color: black;
+  color: black;
   box-shadow: none;
+`;
+
+export const CustomToolbar = styled(Toolbar)`
+  padding: 0px 30px;
+`;
+
+export const CustomCard = styled(Card)`
+  position: absolute;
+  top: 10%;
+  width: 80%;
+  height: 100%;
+  right: 0;
+`;
+
+export const CustomCardHeader = styled(CardHeader)`
+  background: #FBDC8F;
+  height: 40px;
+  span {
+    font-size: 16px;
+    font-weight: bold;
+  }
+`;
+
+export const CustomStack = styled(Stack)`
+  display: block;
+  position: absolute;
+  top: 15%;
+	margin-left: 15px;
+  .settings-button {
+    margin: 0px;
+    display: flex;
+  }
+`;
+
+export const CustomButton = styled(Button)`
+  background: transparent;
+  color: darkgrey;
+  box-shadow: none;
+  text-transform: none;
+  border: none;
 `;
